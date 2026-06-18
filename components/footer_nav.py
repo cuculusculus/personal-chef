7c7f73b254a02fa71f73bc1c0d45339d4a4ea3ba
cuculusculus <myinbox.jp@gmail.com>
2026-06-19 01:04:54 +0900

    Update footer_nav.py

--- components/footer_nav.py
+++ components/footer_nav.py
@@ -7,6 +7,19 @@ from config.constants import *
 
 def render_footer_nav():
 
+    st.markdown("""
+    <style>
+    .nav-pills{
+        position:fixed !important;
+        bottom:0 !important;
+        left:0 !important;
+        width:100% !important;
+        background:white !important;
+        z-index:999999 !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
     st.error("footer loaded")
 
     pages = [
@@ -19,28 +32,16 @@ def render_footer_nav():
     current_idx = pages.index(st.session_state.page)
 
     selected = option_menu(
-    menu_title=None,
-    options=["Home", "Stock", "Fav", "History"],
-    icons=[
-        "house-fill",
-        "basket",
-        "bookmark-star",
-        "clock-history"
-    ],
-    default_index=current_idx,
-    orientation="horizontal"
-)
+        menu_title=None,
+        options=["Home", "Stock", "Fav", "History"],
+        icons=[
+            "house-fill",
+            "basket",
+            "bookmark-star",
+            "clock-history"
+        ],
+        default_index=current_idx,
+        orientation="horizontal"
+    )
 
-st.markdown("""
-<style>
-.nav-pills{
-    position:fixed !important;
-    bottom:0 !important;
-    left:0 !important;
-    width:100% !important;
-    background:white !important;
-    z-index:999999 !important;
-}
-</style>
-""", unsafe_allow_html=True)
     st.write("selected =", selected)
