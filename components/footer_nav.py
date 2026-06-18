c76600f0be8ed8847e32f2686e5c3d4ac6587dbd
cuculusculus <myinbox.jp@gmail.com>
2026-06-19 01:31:10 +0900

    Update footer_nav.py

--- components/footer_nav.py
+++ components/footer_nav.py
@@ -4,47 +4,24 @@ import streamlit as st
 
 def render_footer_nav():
 
-    st.markdown("""
-<style>
-
-/* option_menu本体 */
-
-ul.nav.nav-pills.mb-auto.nav-justified {
-
-    position: fixed !important;
-
-    bottom: 0 !important;
-    left: 0 !important;
-
-    width: 100vw !important;
-
-    background: red !important;
-    border-top: 1px solid #ddd !important;
-
-    z-index: 999999 !important;
-
-    margin: 0 !important;
-    padding: 0 !important;
-}
-
-/* 4分割 */
-
-ul.nav.nav-pills.mb-auto.nav-justified > li {
-    flex: 1 !important;
-    text-align: center !important;
-}
-
-</style>
-""", unsafe_allow_html=True)
-
-    option_menu(
-        None,
-        ["Home", "Stock", "Fav", "History"],
+    selected = option_menu(
+        menu_title=None,
+        options=["Home", "Stock", "Fav", "History"],
         icons=[
             "house-fill",
             "basket",
             "bookmark-star",
             "clock-history"
         ],
-        orientation="horizontal"
+        orientation="horizontal",
+        styles={
+            "container": {
+                "position": "fixed",
+                "bottom": "0",
+                "left": "0",
+                "width": "100%",
+                "background-color": "red",
+                "z-index": "999999"
+            }
+        }
     )
