50ec819c8a90cd957c2d0909f4fd95f50f28ae96
cuculusculus <myinbox.jp@gmail.com>
2026-06-19 01:12:07 +0900

    Update footer_nav.py

--- components/footer_nav.py
+++ components/footer_nav.py
@@ -1,38 +1,10 @@
 # components/footer_nav.py
-
 from streamlit_option_menu import option_menu
 import streamlit as st
 
 from config.constants import *
 
 def render_footer_nav():
-
-    st.markdown("""
-    <style>
-
-    /* option_menu全体 */
-    ul.nav {
-        position: fixed !important;
-        bottom: 0 !important;
-        left: 0 !important;
-        width: 100% !important;
-
-        background: white !important;
-        border-top: 1px solid #ddd !important;
-
-        z-index: 999999 !important;
-        margin: 0 !important;
-        padding: 0 !important;
-    }
-
-    /* 各ボタン均等割り */
-    ul.nav li {
-        flex: 1 !important;
-        text-align: center !important;
-    }
-
-    </style>
-    """, unsafe_allow_html=True)
 
     pages = [
         PAGE_RECIPE,
@@ -42,22 +14,10 @@ def render_footer_nav():
     ]
 
     current_idx = pages.index(st.session_state.page)
-    st.markdown("""
-    <style>
-    
-    ul {
-        border: 3px solid red !important;
-    }
-    
-    nav {
-        border: 3px solid blue !important;
-    }
-    
-    </style>
-""", unsafe_allow_html=True)
+
     selected = option_menu(
         menu_title=None,
-        options=["Home", "Stock", "Fav", "History"],
+        options=[" ", "  ", "   ", "    "],
         icons=[
             "house-fill",
             "basket",
@@ -65,22 +25,34 @@ def render_footer_nav():
             "clock-history"
         ],
         default_index=current_idx,
-        orientation="horizontal"
+        orientation="horizontal",
+        styles={
+            "container": {
+                "padding": "0!important",
+                "margin": "0!important",
+                "background-color": "#ffffff",
+                "border-top": "1px solid #ddd",
+                "position": "fixed",
+                "bottom": "0",
+                "left": "0",
+                "width": "100%",
+                "z-index": "999999"
+            },
+            "nav-link": {
+                "text-align": "center",
+                "margin": "0px",
+                "--hover-color": "#f5f5f5"
+            },
+            "icon": {
+                "font-size": "22px"
+            }
+        }
     )
 
-    page_map = {
-        "Home": PAGE_RECIPE,
-        "Stock": PAGE_STOCK,
-        "Fav": PAGE_FAVORITE,
-        "History": PAGE_HISTORY
-    }
+    idx = [" ", "  ", "   ", "    "].index(selected)
 
-    target = page_map[selected]
+    target = pages[idx]
 
     if target != st.session_state.page:
         st.session_state.page = target
         st.rerun()
-st.markdown("""
----
-footer end
-""")
