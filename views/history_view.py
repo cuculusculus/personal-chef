9dc37433c333c6bf1de4789458cb01f1c2db112d
cuculusculus <myinbox.jp@gmail.com>
2026-06-19 18:22:40 +0900

    Update history_view.py

--- views/history_view.py
+++ views/history_view.py
@@ -6,7 +6,7 @@ from views.recipe_helpers import render_saved_recipe_detail
 # 元通りのシンプルなダイアログ構造に復活
 @st.dialog(":material/kitchen: レシピ詳細")
 def show_recipe_detail_dialog(recipe, mode, idx=None, is_fav=False):
-    
+    scroll_to_top()
     # 詳細表示
     render_saved_recipe_detail(recipe, recipe.get("servings", 2), show_buttons=False)
     
