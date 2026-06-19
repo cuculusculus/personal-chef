50dcecb5f24686ced706448bc946a7b98235cad7
cuculusculus <myinbox.jp@gmail.com>
2026-06-20 00:57:04 +0900

    Update history_view.py

--- views/history_view.py
+++ views/history_view.py
@@ -61,7 +61,7 @@ def show_recipe_detail_dialog(recipe, mode, idx=None, is_fav=False):
 # お気に入り画面
 def render_favorite_page():
 
-    st.subheader(":material/bookmark_star: お気に入りレシピ")
+    st.subheader(":material/bookmark_star: お気に入り")
 
     if not st.session_state.favorite_recipes:
         st.info("お気に入りに登録されたレシピはまだありません。")
@@ -84,7 +84,7 @@ def render_favorite_page():
 # 履歴画面
 def render_history_page():
 
-    st.subheader(":material/overview: 過去の提案履歴")
+    st.subheader(":material/overview: 履歴")
 
     if not st.session_state.recipe_history:
         st.info("過去に提案されたレシピ履歴はありません。")
