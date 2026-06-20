2f204df64263b6f23105efbb4c56ac04cbe6ef37
cuculusculus <myinbox.jp@gmail.com>
2026-06-20 20:28:36 +0900

    Update recipe_view.py

--- views/recipe_view.py
+++ views/recipe_view.py
@@ -157,9 +157,10 @@ def render_recipe_page():
     # )
 
     recipe = st.session_state.get("current_recipe_obj")
-    st.write("現在人数:", st.session_state["servings_input"])
-st.write("保存人数:", recipe.get("servings"))
+    
     if recipe:
+        st.write("現在人数:", st.session_state["servings_input"])
+        st.write("保存人数:", recipe.get("servings"))
         st.write("---")
         st.subheader(":material/menu_book: レシピ・工程")
         with st.container(border=True):
