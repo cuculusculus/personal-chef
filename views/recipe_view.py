2fd4645857b45e484580d001922c9bd9da79cad6
cuculusculus <myinbox.jp@gmail.com>
2026-06-22 00:45:43 +0900

    Update recipe_view.py

--- views/recipe_view.py
+++ views/recipe_view.py
@@ -72,7 +72,7 @@ def render_recipe_page():
     seasonings_stock = list(st.session_state["stock_data"]["seasonings"].keys())
 
     if not all_stock_formatted:
-        st.info(":material/shopping_cart: まずは下部メニューの「食材」ページから冷蔵庫の中身を登録してね！")
+        st.info(":material/kitchen: まずは下部メニューの「食材」ページから冷蔵庫の中身を登録してね！")
         return
     
     if st.button("登録している食材を確認", icon=":material/frame_inspect:", use_container_width=True):
