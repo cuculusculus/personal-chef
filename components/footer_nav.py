3643a70184299c7ff92408ec35d03876fe7cf0b3
cuculusculus <myinbox.jp@gmail.com>
2026-06-24 22:53:14 +0900

    Update footer_nav.py

--- components/footer_nav.py
+++ components/footer_nav.py
@@ -16,9 +16,9 @@ def render_footer_nav():
     current_page = st.session_state.get("page", PAGE_RECIPE)
 
     query_params = st.query_params
-    if "page" in query_params and query_params["page"] != current_page:
-        st.session_state.page = query_params["page"]
-        st.rerun()
+    #if "page" in query_params and query_params["page"] != current_page:
+        #st.session_state.page = query_params["page"]
+        #st.rerun()
 
     nav_config = [
         {"page": PAGE_RECIPE, "icon": "restaurant"},
